6075369426948c60cff9b74d91937ddec6033d71
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-03-04 09:03:02 +0700

    initial commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Testing</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="bg-secondary">
+	<div class="container my-5">
+		<div class="row">
+			<div class="col-md-4 offset-md-4 bg-white shadow-sm p-5 mt-5">
+				<h4 class="text-center">Login Administrator</h4>
+				<form method="post">
+					<div class="mb-3">
+						<label>Email</label>
+						<input type="email" name="email" class="form-control">
+					</div>
+					<div class="mb-3">
+						<label>Password</label>
+						<input type="password" name="password" class="form-control">
+					</div>
+					<button class="btn btn-secondary">Login</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+		crossorigin="anonymous"></script>
+</body>
+
+</html>
